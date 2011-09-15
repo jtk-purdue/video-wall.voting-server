849d8702dbaef5c72ed337c7f39b8c2dab46fab5
rick.fogle90 <rick.fogle90@localhost>
2011-09-14 20:35:53 -0400

    Added delay for sending triggers and changed vote items for demo

--- src/Server.java
+++ src/Server.java
@@ -10,27 +10,35 @@ public class Server{
 	NecManager n;
 	String password;
 	ConnectionManager connections;
+	boolean isActive;
 	
-	Server(boolean isActive, String password) {
+	Server(boolean isActive, String password, int secondsToWait) {
 		list = new ListManager(false);
 		brodcast = new Brodcaster();
 		n = new NecManager(isActive);
+		this.isActive = isActive;
 		this.password = password;
 	}
 	void run()
 	{		
-		list.add("CNN","1","cnn");
-		list.add("ESPN","2","espn");
-		list.add("ABC","3","abc");
-		list.add("Nickelodeon","4", "nickelodeon");
-		list.add("Comedy Central","5", "comedyCentral");
-		list.add("Sci Fi","6", "SciFi");
-		list.add("Weather Channel", "7", "weather");
-		list.add("National Geographic", "8","nationalGeographic");
+//		list.add("CNN","1","cnn");
+//		list.add("ESPN","2","espn");
+//		list.add("ABC","3","abc");
+//		list.add("Nickelodeon","4", "nickelodeon");
+//		list.add("Comedy Central","5", "comedyCentral");
+//		list.add("Sci Fi","6", "SciFi");
+//		list.add("Weather Channel", "7", "weather");
+//		list.add("National Geographic", "8","nationalGeographic");
+		
+		list.add("CS Camp", "1", "cscamp");
+		list.add("Double Take", "2", "dblTake");
+		list.add("Hot Seat", "3", "hotSeat");
+		list.add("Jason Lestina", "4", "jason");
+		list.add("Mixable", "5", "mixable");
 		
 		connections = new ConnectionManager();
 		
-		v = new voteThread(list, brodcast, connections);
+		v = new voteThread(list, brodcast, connections, isActive);
 		v.start();
 		
 		
@@ -63,6 +71,7 @@ public class Server{
 		String s2;
 		boolean mode = false;
 		int loc;
+		int secondsToWait = 0;
 		for(int i=0; i < args.length; i++){
 			s = args[i];
 			if(s.contains("=")){
@@ -78,12 +87,19 @@ public class Server{
 					}
 					
 					System.out.println("mode is"+ s2);
+				}else if(s1.equals("wait")){
+					try{
+						secondsToWait = Integer.parseInt(s2);
+					} catch (Exception e){
+						System.out.println("poorly formed wait command");
+					}
+					System.out.println("wait time is: "+ secondsToWait);
 				}
 			}
 		}
 		
 		
-		server = new Server(mode, password);
+		server = new Server(mode, password, secondsToWait);
 		server.run();
 	}
 
--- src/voteThread.java
+++ src/voteThread.java
@@ -4,43 +4,66 @@
 public class voteThread extends Thread {
 	ListManager list;
 	Brodcaster brodcast;
-	long startTime = System.currentTimeMillis();
+	long lastChange;
 	ConnectionManager connections;
-	voteThread(ListManager list, Brodcaster brodcast, ConnectionManager connections) {
+	boolean isActive;
+	
+	voteThread(ListManager list, Brodcaster brodcast, ConnectionManager connections, boolean isActive) {
 		this.list = list;
 		this.brodcast = brodcast;
 		this.connections = connections;
+		this.isActive = isActive;
+		lastChange = System.currentTimeMillis()- 30000;
 	}
 	public void run(){
 		boolean changed = false;
+		boolean needToChange = false;
 		String first[];
 		String second[];
 		first = list.getStringList();
 		while(true){
 				
-				changed = list.sortVote();
-				if(changed){
-					second = list.getStringList();
-					for(int i=0; i < second.length; i++){
-						if(!second[i].equals(first[i])){
-							sendRank(second[i], i);
-						}
+			changed = list.sortVote();
+			if(changed){
+				needToChange = true;
+				second = list.getStringList();
+				for(int i=0; i < second.length; i++){
+					if(!second[i].equals(first[i])){
+						sendRank(second[i], i);
 					}
-					first = second;
-					changed = false;
-					System.out.println("Sending trigger for item: "+ list.getVote(0).id);
-					//brodcast.sendAll(list.getVote(0).trigger);
 				}
-				
-				try {
-					synchronized(this){
-						wait();
-					}
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			 // TODO: set-up decay algorithm
+				first = second;
+				changed = false;
+			}
 			
+			if((System.currentTimeMillis() - lastChange) >= 30000){
+				if(needToChange){
+					
+					if(isActive){
+						brodcast.sendAll(list.getVote(0).trigger);
+					}else{
+						System.out.println("Sending trigger for item: "+ list.getVote(0).id);
+					}
+					needToChange = false;
+					lastChange = System.currentTimeMillis();
+				} else {
+					try{
+						synchronized(this){
+							wait();
+						}
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			} else {
+				synchronized(this){
+					try {
+						wait(30000 - (System.currentTimeMillis() - lastChange));
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
 		}
 	}
 	
